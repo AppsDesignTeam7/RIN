093903e0938f0832aaea08c58bbfda200ab1a6ff
mng16 <mairi.ng.16@ucl.ac.uk>
2016-12-02 21:37:25 +0000

    Add files via upload

--- _scripts/script.js
+++ _scripts/script.js
@@ -3,10 +3,31 @@ var courses = document.getElementById("courses");
 var conferences = document.getElementById("conferences");
 var expandedSearch = document.getElementById("expandedSearch");
 var searchField = document.getElementById("searchField");
-var favouriteButton = document.getElementsByClassName("favouriteButton");
+//var favouriteButton = document.getElementsByClassName("favouriteButton");
+var calendarButton = document.getElementsByClassName("calendarButton");
+var favouriteStar = document.getElementsByClassName("fa.fa-star");
 
 window.onload = function () {
-   $(".accordion").accordion();
+   $(".accordion").accordion({
+// sets accordion to be unexpanded at start (rather than default of having one section expanded at start)
+   active: false,
+   collapsible: true,
+   heightStyle: "content"
+   });
+ 
+   $( ".accordion .fa.fa-star").click(function(event) {
+       event.stopPropagation();
+        event.preventDefault();
+        $(this).css("color","Gold");
+        alert("Added to Favourites");
+        });
+   
+   $(".accordion .fa.fa-calendar").click(function (event) {
+        event.stopPropagation();
+        event.preventDefault();
+        $(this).css("color","DarkBlue");
+        alert("Added to calender");
+        });
    
    seminars.style.display = "block";
    courses.style.display = "none";
@@ -56,12 +77,10 @@ document.getElementById("searchButton").onclick = function () {
     expandedSearch.style.display = "none";
 };
 
-favouriteButton.onclick = function () {
+/*
+favouriteStar.onclick = function () {
 
-    alert("Added to favourites");
-};
-
-
-
-
+    favouriteStar.color = "blue";
+    
+};*/
 
