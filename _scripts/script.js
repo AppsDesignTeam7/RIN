de16f059c02d23cc4c6dbe604a7968398d7656d7
mng16 <mairi.ng.16@ucl.ac.uk>
2017-01-08 12:04:34 +0000

    Add files via upload

--- _scripts/script.js
+++ _scripts/script.js
@@ -23,18 +23,6 @@ heightStyle: "content",
 });
 
 
- 
-
-
-// when the favourite star is clicked, the normal expansion function of the accordion is stoppped (this is to allow the star area of the accordion to be clickable)
-$( ".accordion .fa.fa-star").click(function(event) {
-   event.stopPropagation();
-    event.preventDefault();
-    $(this).css("color","Gold"); /* changes favourite star colour to gold on click */
-    alert("Added to Favourites");
-    });
-
-
 // stops the normal expansion function of the accordion when when the  calendar button is clicked.
 $(".accordion .fa.fa-calendar").click(function (event) {
     event.stopPropagation();
@@ -57,10 +45,10 @@ $(".eventSummarySection").click(function() {
     initMap();
 });
 
-/*seminars.style.display = "block";*/
-/*courses.style.display = "none";
-conferences.style.display = "none";
-expandedSearch.style.display = "none";*/
+/*seminars.style.display = "block";
+courses.style.display = "none";
+conferences.style.display = "none";*/
+expandedSearch.style.display = "none";
 };
 
 /*
