34006d94f8460a6ddbd44850cc91036762098787
tOOtl <tOOtl@users.noreply.github.com>
2017-01-14 11:59:06 +0000

    Edited to handle Clear Filters button

--- _php/applyFilters.php
+++ _php/applyFilters.php
@@ -2,25 +2,33 @@
 
 session_start();
 
-$tags = array();
-// Create an array of the TagNumber of all selected filters
-$selected = $_POST['tagCheckbox'];
-foreach ($selected as $tag) {
-	array_push($tags, $tag);
-}
-
-// If tags have been selected, save them. If none are selected, then clear the selection.
-
-if (sizeof($tags) > 0) {
-	$_SESSION['selectedTags'] = $tags;
-} else {
+if ($_POST['submitBtn'] == "clear") {
+	// If they pressed clear, remove all filters
 	unset($_SESSION['selectedTags']);
+} else {
+	// If they pressed apply, set the filters for sql queries
+	$tags = array();
+	// Create an array of the TagNumber of all selected filters
+	$selected = $_POST['tagCheckbox'];
+	foreach ($selected as $tag) {
+		array_push($tags, $tag);
+	}
+
+	// If tags have been selected, save them. If none are selected, then clear the selection.
+	if (sizeof($tags) > 0) {
+		$_SESSION['selectedTags'] = $tags;
+	} else {
+		unset($_SESSION['selectedTags']);
+	}
 }
+
+// Get the location to return to
+if (isset($_SESSION['searchLocation'])) {
+	$destination = $_SESSION['searchLocation'];
+}	
 
 // reload page with new tag selections
-// ref: Kiel Labuca https://stackoverflow.com/questions/21226166/php-header-location-redirect-not-working
-// UPDATE THE ADDRESS USED HERE ONCE SITE IS LIVE
-echo "<script type='text/javascript'> document.location = 'http://localhost:8887/index.php'; </script>";
+include('redirect.php');
 die();
 
 ?>
